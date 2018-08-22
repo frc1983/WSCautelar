7aa7832b0ca896beba6d68bf8e3696eaaa8b0cd5
frc1983 <fabiorochadacosta@gmail.com>
2018-08-22 09:34:24 -0300

    dsa

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var express = require('express'),
   app = express(),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  db = require('./database/index');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.engine('html', require('ejs').renderFile);
 
 require('./app/controllers/index')(app);
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.get('/', function (req, res) {
+  if (db) {
+    res.render('index.html', { info: "Ok" });
+  } else {
+    res.render('index.html', { info: "Error" });
+  }
+});
 
 // error handling
 app.use(function (err, req, res, next) {
