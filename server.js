01ceee0a69c5fa7d0c19fb362bcfec2ace37907d
frc1983 <fabiorochadacosta@gmail.com>
2018-08-22 09:23:56 -0300

    Limpeza

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
 var express = require('express'),
   app = express(),
-  bodyParser = require('body-parser'),
-  db = require('./database/index');
+  bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.engine('html', require('ejs').renderFile);
 
 require('./app/controllers/index')(app);
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.get('/', function (req, res) {
-  if (db) {
-    res.render('index.html', { pageCountMessage: 123, dbInfo: "fabio" });
-  } else {
-    res.render('index.html', { pageCountMessage: null });
-  }
-});
-
 // error handling
 app.use(function (err, req, res, next) {
   console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+  res.status(500).send('Server error!');
 });
 
 app.listen(port, ip);
