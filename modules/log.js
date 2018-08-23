4af6401231c9659a4c6463fc5eb669be6acf8920
frc1983 <fabiorochadacosta@gmail.com>
2018-08-23 14:25:16 -0300

    import mailer

--- modules/log.js
+++ modules/log.js
@@ -1,6 +1,4 @@
-const path = require('path');
-const nodemailer = require('nodemailer');
-const hbs = require('nodemailer-express-handlebars');
+const mailer = require('../../modules/mailer');
 
 const { mailLogTo, mailLogFrom } = require('../config/mail.json');
 
